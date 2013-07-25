68ebff05d54df0726ffc56690d517d53d2b849e8
Ikuo Terado <eqobar@gmail.com>
2013-07-25 20:05:22 +0900

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('mindmapApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
