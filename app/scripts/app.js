50d2803340da4ce90df0a6138d894310a1ae3612
Ikuo Terado <eqobar@gmail.com>
2013-08-11 00:08:39 +0900

    Added the first list view

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,6 +4,10 @@ angular.module('mindmapApp', [])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
+        templateUrl: 'views/list.html',
+        controller: 'ListCtrl'
+      })
+      .when('/:id', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
