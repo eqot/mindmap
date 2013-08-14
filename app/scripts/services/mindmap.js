c52ac2ecbcd541b9cb7e24fbdbc654759aa1baa8
Ikuo Terado <eqobar@gmail.com>
2013-08-15 04:00:55 +0900

    Added local db server for debugging

--- app/scripts/services/mindmap.js
+++ app/scripts/services/mindmap.js
@@ -3,6 +3,7 @@
 angular.module('mindmapApp')
   .factory('MindMap', function ($resource) {
     var DB_URL = '//192.168.33.10\\:3000/mindmaps/mindmaps/';
+    // var DB_URL = '//localhost\\:3000/mindmaps/mindmaps/';
     return $resource(DB_URL + ':id', {id: '@_id'}, {
       'update': {method: 'PUT'}
     });
